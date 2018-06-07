394be47bec04a8de4cdb54e952fb39867ca00bed
Jason J Kraft <jasonjkraft@gmail.com>
2018-06-07 14:25:29 -0700

    performing well on sample data

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org' do
+  # gem 'nokogiri'
+  gem 'activesupport'
+
+  group :development, :test do
+    gem 'byebug'
+    gem 'pry'
+    gem 'pry-byebug'
+  end
+end
--- convert.rb
+++ convert.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'csv'
 require 'time'
 require 'pry-byebug'
 require 'active_support/inflector'
@@ -6,28 +7,56 @@ require 'active_support/inflector'
 
 ## 
 # bundle exec ruby json_to_xml.rb Rick\ and\ Morty.json; cat Rick\ and\ Morty.xml | xmllint --format -
+# TODO: re-add <item type="tweet">
 
 def run
-  filepath = ARGV[0]
-  file = File.new(filepath, "r")
-  json = JSON.load(file)
+  @output_array_scalar_as_node_element = false
 
-  xml = nil
+  filepath = ARGV[0]
   ext = File.extname(filepath)
-  if ext == 'json'
+
+  xml = if ext == '.json'
+    file = File.new(filepath, "r")
+    json = JSON.load(file)
     if json['thread']
-      xml = make_tag('root') do
+      make_tag('root') do
         recurse(json['thread'], 1, 'items')
       end
-    elsif ["http://twitter.com", 'reddit'].any? { |s| json['source'].downcase.include? s }
-      xml = make_tag('root') do
+    elsif json['source'] && ["http://twitter.com", 'reddit'].any? { |s| json['source'].downcase.include? s }
+      make_tag('root') do
         recurse([json], 1, 'items')
       end
+    else
+      make_tag('root') do
+        recurse(json)
+      end
     end
-  elsif ext == 'csv'
+  elsif ext == '.csv'
+    @output_array_scalar_as_node_element = true
     make_tag('root') do
-      CSV.read(filepath, headers: true).reduce do |_,row|
-        _ + recurse(row.to_h.select { |k,_| !k.nil? }) # remove nil keys
+      CSV.read(filepath, headers: true).reduce('') do |output,row|
+        # TODO: consider colocating this value extrusion with date/encoding value 
+        # output in #recurse
+        dict = row.each_with_object({}) do |(header,field), obj|
+          next if header.nil?
+          if field && field.include?(';')
+            # fields containing "Title1: val,val; Title2: val,val;.."
+            # become title1: [val, val], title2: [val, val]
+            key_values = {}
+            field.split(';').each do |kv_pair|
+              k,v = kv_pair.split(':')
+              key_values[k.pluralize] = v.split(',')
+            end
+            field = key_values
+          elsif field && field.include?(',')
+            # comma-separated fields become arrays
+            field = field.split(',')
+          end
+          obj[header] = field
+        end
+        output + make_tag('item') do
+          recurse(dict)
+        end
       end
     end
   end
@@ -38,7 +67,7 @@ end
 
 def recurse(o, depth=0, tag=nil, parent=nil)
   if o.is_a?(Hash)
-    out = parent == :array ? "<#{tag.singularize}>" : ''
+    out = parent == :array ? "<#{clean_tag(tag).singularize}>" : ''
     o.each do |k,v|
       k_clean = clean_tag(k) # make sure tag name is valid
       if k.include?('$')
@@ -50,19 +79,22 @@ def recurse(o, depth=0, tag=nil, parent=nil)
         end
       end
     end
-    out += parent == :array ? "</#{tag.singularize}>" : ''
+    out += parent == :array ? "</#{clean_tag(tag).singularize}>" : ''
     out
   elsif o.is_a?(Array)
-    o.reduce { |_.v| _ + recurse(v, depth+1, tag, :array) }
+    o.reduce('') { |_,v| _ + recurse(v, depth+1, tag, :array) }
   else
     # value output
-    if ['date', 'created_at'].include? tag
+    out = @output_array_scalar_as_node_element && parent == :array ? "<#{clean_tag(tag).singularize}>" : ''
+    out += if ['date', 'created_at'].include? tag
       # attempt to unify dates
       DateTime.parse(o).iso8601
     else
       # encode special characters
-      "#{o}".gsub(/\&/, '&amp;')
+      "#{o}".gsub(/\&/, '&amp;').strip
     end
+    out += @output_array_scalar_as_node_element && parent == :array ? "</#{clean_tag(tag).singularize}>" : ''
+    out
   end
 end
 
@@ -74,7 +106,7 @@ def make_tag(name, attrs=nil)
 end
 
 def clean_tag(k)
-  k = k[1..-1] if k[0] == '_' || k[0] == '$'
+  k = k[1..-1] if k[0] == '_' || k[0] == '$' || k[0] == ' '
   k.gsub(/\ /, '_').underscore
 end
 
