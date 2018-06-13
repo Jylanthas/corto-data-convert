dad5bb159a51ed60ec8e10adebaba4785aeb027f
Jason J Kraft <jasonjkraft@gmail.com>
2018-06-13 12:08:09 -0700

    allow running source/output on directories fix a couple bugs add progress bar refactoring

--- convert.rb
+++ convert.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'json'
 require 'csv'
+require 'nokogiri'
 require 'time'
 require 'pry-byebug'
 require 'active_support/inflector'
@@ -8,30 +9,65 @@ require 'active_support/inflector'
 # TODO: re-add <item type="tweet">
 
 def run
-  @output_array_scalar_as_node_element = false
-
   filepath = ARGV[0]
+  outpath = ARGV[1]
+  validate = ARGV[2] && ARGV[2] == 'true'
+
+  if File.directory?(filepath)
+    paths = Dir[File.join(filepath, '/**/*.{json,csv}')]
+    puts "#{paths.length} files"
+    paths.each_with_index do |path,i|
+      perc = 100 * (i.to_f / paths.length)
+      progress = "=" * (perc/5) unless perc < 5
+      printf("\r: [%-20s] %.2f%% %s", progress, perc, spinner.next)
+
+      # default: write sibling file to base dir of filepath
+      # outpath dir given: write outpath dir + path remaining after filepath base dir
+      outfile = if outpath.nil?
+          File.dirname(path)
+        else
+          File.join(outpath, path[filepath.length..-1]).tap do |p|
+            FileUtils.mkdir_p(File.dirname(p))
+          end
+        end
+      convert(path, outfile, validate)
+    end
+  else
+    # outfile = if outpath.nil?
+    #     File.dirname(filepath)
+    #   else
+    #     binding.pry
+    #     File.join(outpath, outpath[filepath.length..-1]).tap do |p|
+    #       FileUtils.mkdir_p(File.dirname(p))
+    #     end
+    #   end
+    convert(filepath, outpath, validate)
+  end
+end
+
+def convert(filepath, outpath=nil, validate=false)
+  @output_array_scalar_as_node_element = false
   ext = File.extname(filepath)
 
   xml = if ext == '.json'
     file = File.new(filepath, "r")
     json = JSON.load(file)
     if json['thread']
-      make_tag('root') do
+      make_element('root') do
         recurse(json['thread'], 1, 'items')
       end
     elsif json['source'] && ["http://twitter.com", 'reddit'].any? { |s| json['source'].downcase.include? s }
-      make_tag('root') do
+      make_element('root') do
         recurse([json], 1, 'items')
       end
     else
-      make_tag('root') do
+      make_element('root') do
         recurse(json)
       end
     end
   elsif ext == '.csv'
     @output_array_scalar_as_node_element = true
-    make_tag('root') do
+    make_element('root') do
       CSV.read(filepath, headers: true).reduce('') do |output,row|
         # TODO: consider colocating this value extrusion with date/encoding value 
         # output in #recurse
@@ -43,7 +79,7 @@ def run
             key_values = {}
             field.split(';').each do |kv_pair|
               k,v = kv_pair.split(':')
-              key_values[k.pluralize] = v.split(',')
+              key_values[k.pluralize] = v && v.split(',')
             end
             field = key_values
           elsif field && field.include?(',')
@@ -52,60 +88,112 @@ def run
           end
           obj[header] = field
         end
-        output + make_tag('item') do
+        output + make_element('item') do
           recurse(dict)
         end
       end
     end
   end
       
-  filename = File.basename(filepath, ext)
-  File.write("#{filename}.xml", xml)
+  outpath = if outpath.nil?
+    File.basename(filepath, ext) + '.xml' # current dir
+  else
+    if File.directory?(outpath) # directory (outpath) + filename.xml
+      File.join(outpath, File.basename(filepath, ext) + '.xml')
+    else # directory (file) + filename.xml
+      File.join(File.dirname(outpath), File.basename(filepath, ext) + '.xml')
+    end
+  end
+  File.write(outpath, xml)
+  
+  if validate
+    errors = Nokogiri::XML(xml).errors
+    unless errors.empty?
+      puts filepath
+      puts outpath
+      puts errors
+    end
+  end
 end
 
-def recurse(o, depth=0, tag=nil, parent=nil)
+def recurse(o, depth=0, parent_element=nil, parent_type=nil)
   if o.is_a?(Hash)
-    out = parent == :array ? "<#{clean_tag(tag).singularize}>" : ''
-    o.each do |k,v|
-      k_clean = clean_tag(k) # make sure tag name is valid
+    out = o.reduce('') do |mem, (k,v)|
+      k_clean = clean_name(k) # make sure tag name is valid
       if k.include?('$')
         # don't output tag, this node is meta type info
-        out += recurse(v, depth+1, k_clean)
+        mem + recurse(v, depth+1, k_clean)
       else
-        out += make_tag(k_clean) do
+        mem + make_element(k_clean) do
           recurse(v, depth+1, k_clean)
         end
       end
     end
-    out += parent == :array ? "</#{clean_tag(tag).singularize}>" : ''
-    out
+    
+    if parent_type == :array
+      make_element(clean_name(parent_element).singularize) do # creates child element <things><thing></thing></things>
+        out
+      end
+    else
+      out
+    end
   elsif o.is_a?(Array)
-    o.reduce('') { |_,v| _ + recurse(v, depth+1, tag, :array) }
+    o.reduce('') { |_,v| _ + recurse(v, depth+1, parent_element, :array) }
   else
     # value output
-    out = @output_array_scalar_as_node_element && parent == :array ? "<#{clean_tag(tag).singularize}>" : ''
-    out += if ['date', 'created_at'].include? tag
-      # attempt to unify dates
-      DateTime.parse(o).iso8601
+    text = if ['date', 'created_at'].any? { |k| parent_element.include?(k) }
+      # attempt to unify dates, if parse fails, leave it be
+      DateTime.parse(o).iso8601 rescue clean_text(o)
     else
-      # encode special characters
-      "#{o}".gsub(/\&/, '&amp;').strip
+      clean_text(o)
     end
-    out += @output_array_scalar_as_node_element && parent == :array ? "</#{clean_tag(tag).singularize}>" : ''
-    out
+
+    if @output_array_scalar_as_node_element && parent_type == :array
+      make_element(clean_name(parent_element).singularize) do
+        text
+      end
+    else
+      text
+    end
   end
 end
 
-def make_tag(name, attrs=nil)
+def make_element(name, attrs=nil)
   attr_str = attrs.reduce('') { |_,(k,v)| _+" #{k}='#{v}'" } if attrs
   "<#{name}#{attr_str}>"+
   yield +
   "</#{name}>"
 end
 
-def clean_tag(k)
-  k = k[1..-1] if k[0] == '_' || k[0] == '$' || k[0] == ' '
-  k.gsub(/\ /, '_').underscore
+def clean_name(name)
+  name = name.gsub(/[ ,-]/, '_')
+  name = name.gsub(/#/, 'num')
+  name = name.gsub(/%/, 'pct')
+  name = name.gsub(/[^a-zA-Z0-9_]/, '')
+  name = name[1..-1] if name[0] == '_'
+  name = name.gsub(/(^[0-9]+$)/, 'el\1') # numerical elements { "25" => val } becomes <el25></el25>
+  name
+end
+
+def clean_text(text)
+  text = "#{text}".gsub("\u001A", '').strip # remove special characters
+  if text.include?('<') && text.include?('>') # seems to include xml (or html). comment out, don't embed as-is
+    "<!--#{text}-->"
+  else
+    # "#{o}".encode(xml: :text).gsub("\u001A", '').strip
+    text.encode(xml: :text)
+  end
+end
+
+def spinner
+  @spinner ||= Enumerator.new do |e|
+    loop do
+      e.yield '|'
+      e.yield '/'
+      e.yield '-'
+      e.yield '\\'
+    end
+  end
 end
 
 class String
